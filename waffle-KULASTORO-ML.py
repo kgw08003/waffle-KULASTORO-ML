c40c132eb64df78bc830c7259e062acf247d4c19
Las <153968515+MinwooPyeon@users.noreply.github.com>
2024-03-09 12:48:22 +0900

    Add files via upload

--- waffle-KULASTORO-ML.py
+++ waffle-KULASTORO-ML.py
@@ -6,24 +6,28 @@ import pandas as pd
 import numpy as np
 
 columns = ['id','text','label']
-train_data = pd.read_csv('ratings_train.txt', sep='\t', names=columns, skiprows=1).dropna()
-test_data = pd.read_csv('ratings_test.txt', sep='\t', names=columns, skiprows=1).dropna()
+train_data = pd.read_csv('data/ratings_train.txt', sep='\t', names=columns, skiprows=1).dropna()
+test_data = pd.read_csv('data/ratings_test.txt', sep='\t', names=columns, skiprows=1).dropna()
 
 train_data.to_csv('train_data.csv',index=False)
 test_data.to_csv('test_data.csv',index=False)
 
+# 랜덤 시드 고정
 SEED = 1234
 random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 
+# bert-base-multilingual-cased 토크나이저를 사용
 from transformers import BertTokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
 
+# 토큰화
 tokens = tokenizer.tokenize('내일은 드디어 주말이 시작되는 날입니다.')
 indexes = tokenizer.convert_tokens_to_ids(tokens)
 
+# 토큰 학습
 init_token = tokenizer.cls_token
 eos_token = tokenizer.sep_token
 pad_token = tokenizer.pad_token
@@ -33,15 +37,16 @@ init_token_idx = tokenizer.convert_tokens_to_ids(init_token)
 eos_token_idx = tokenizer.convert_tokens_to_ids(eos_token)
 pad_token_idx = tokenizer.convert_tokens_to_ids(pad_token)
 unk_token_idx = tokenizer.convert_tokens_to_ids(unk_token)
+max_input_length = tokenizer.max_model_input_sizes['bert-base-multilingual-cased']
 
-max_input_length = 128
+# 토크나이저의 문장 시작 토큰과 마지막 토큰을 제거
 def tokenize_and_cut(sentence):
     tokens = tokenizer.tokenize(sentence)
     tokens = tokens[:max_input_length-2]
     return tokens
 
+# 필드 정의
 from torchtext import data
-
 TEXT = data.Field(batch_first = True,
                  use_vocab = False,
                  tokenize = tokenize_and_cut,
@@ -52,6 +57,7 @@ TEXT = data.Field(batch_first = True,
                  unk_token = unk_token_idx)
 LABEL = data.LabelField(dtype = torch.float)
 
+# 데이터 분리
 fields = {'text': ('text',TEXT), 'label': ('label',LABEL)}
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -70,8 +76,8 @@ train_data, valid_data = train_data.split(random_state=random.seed(SEED))
 
 LABEL.build_vocab(train_data)
 
+# 이터레이터 생성
 BATCH_SIZE = 128
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
@@ -83,11 +89,10 @@ train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
 
 # 모델 생성
 from transformers import BertModel
-
 bert = BertModel.from_pretrained('bert-base-multilingual-cased')
 
+# 모델 정의
 import torch.nn as nn
-
 class BERTGRUSentiment(nn.Module):
     def __init__(self, bert, hidden_dim, output_dim,
                 n_layers, bidirectional, dropout):
@@ -123,7 +128,8 @@ class BERTGRUSentiment(nn.Module):
         #output = [batch_size, out_dim]
 
         return output
-    
+
+# 하이퍼파라미터 지정    
 HIDDEN_DIM = 256
 OUTPUT_DIM = 1
 N_LAYERS = 2
@@ -136,7 +142,93 @@ model = BERTGRUSentiment(bert, HIDDEN_DIM, OUTPUT_DIM,
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)    
 
+# bert 모델 훈련 x
 for name, param in model.named_parameters():
-
     if name.startswith('bert'):
         param.requires_grad = False
+        
+# 모델 훈련
+import torch.optim as optim
+optimizer = optim.Adam(model.parameters())
+criterion = nn.BCEWithLogitsLoss()
+
+model = model.to(device)
+criterion = criterion.to(device)
+
+def binary_accuracy(preds, y):
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds==y).float()
+    acc = correct.sum() / len(correct)
+    return acc
+
+# train 함수 정의
+def train(model, iterator, optimizer, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.train()
+    
+    for batch in iterator:
+        optimizer.zero_grad()
+        predictions = model(batch.text).squeeze(1) # output_dim = 1
+        loss = criterion(predictions, batch.label)
+        acc = binary_accuracy(predictions, batch.label)
+        
+        loss.backward()
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+def evaluate(model, iterator, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.eval()
+    
+    with torch.no_grad():
+        for batch in iterator:
+            predictions = model(batch.text).squeeze(1)
+            loss = criterion(predictions, batch.label)
+            acc = binary_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+# 시간 확인 함수
+import time
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+  
+# 토크나이저 설정에서 attention_mask 생성
+tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', 
+                                          attention_mask=True)
+
+# transformer 모델 훈련
+N_EPOCHS = 5
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+    start_time = time.time()
+    
+    train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+    valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+    
+    end_time = time.time()
+
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'tut6-model.pt')
+    
+    print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
